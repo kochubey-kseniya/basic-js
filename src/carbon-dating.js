b020beec37102b3222f61f26a2ad6b613cc927cd
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-05-01 02:09:40 +0300

    refactor: add solution in carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+ let k = 0.693/HALF_LIFE_PERIOD;
 
+  if (typeof sampleActivity === 'string' &&
+    (parseInt(sampleActivity)) &&
+    (sampleActivity > 0 && sampleActivity <= 15)) {
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
+  } else {
+    return false;
+  }
+};
 module.exports = {
   dateSample
 };
