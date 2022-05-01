cc4d773da2f31ab9437753ea3925ed1655933f0a
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-05-01 11:43:51 +0300

    refactor: update file

--- src/what-season.js
+++ src/what-season.js
@@ -12,14 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
- if (!date) return "Unable to determine the time of year!";
-  if (Object.prototype.toString.call(date) !== "[object Date]")
-    throw new Error("Error");
-  let month = date.getMonth();
-  if (month <= 1 || month === 11) return "winter";
-  if (month >= 2 && month <=4) return "spring";
-  if (month >= 5 && month <=7) return "summer";
-  if (month >= 8 && month <=10) return "autumn";
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (isNaN(Date.parse(date))) {
+    throw new Error('Invalid date!')
+  }
+  try {
+    date.getUTCSeconds()
+  } catch { throw new Error('Invalid date!') };
+  const month = date.getMonth();
+  if (month <= 1 || month === 11) return 'winter';
+  if (month >= 2 && month <= 4) return 'spring';
+  if (month >= 5 && month <= 7) return 'summer';
+  if (month >= 8 && month <= 10) return 'autumn';
+  throw new Error('Invalid date!');
 }
 
 module.exports = {
