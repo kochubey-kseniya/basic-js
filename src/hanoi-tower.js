573f2776a5579300caed8b7f0f8693dee2ac9d26
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-05-01 10:34:46 +0300

    refactor: update file

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
+function calculateHanoi(disksNumber, turnsSpeed) {
    let turns = Math.pow(2, disksNumber) - 1;
   let seconds = Math.floor((turns * 3600) / turnsSpeed);
   return { turns, seconds };
