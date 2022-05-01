89bcef14a5f14b6934081a632d610943bd2637e6
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-05-01 13:01:51 +0300

    refactor: update file

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+ if (!Array.isArray(arr)) {
+    throw Error ();
+  }
+
+  let transformArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (arr[i]) {
+      case ('--discard-prev'):
+        if (i === 0) {
+          break;
+        }
+        if (arr[i - 2] !== '--discard-next') {
+          transformArray.pop();
+        }
+        break;
+
+      case ('--discard-next'):
+        if (i === arr.length - 1) {
+          break;
+        } else {
+          i++;
+        }
+        break;
+
+      case ('--double-prev'):
+        if (i === 0) {
+          break;
+        }
+        if (arr[i - 2] !== '--discard-next') {
+          transformArray.push(arr[i - 1]);
+        }
+        break;
+
+      case ('--double-next'):
+        if (i === arr.length - 1) {
+          break;
+        } else {
+          transformArray.push(arr[i + 1]);
+        }
+        break;
+
+      default:
+        transformArray.push(arr[i]);
+    }
+  }
+  
+  return transformArray;
+};
 
 module.exports = {
   transform
